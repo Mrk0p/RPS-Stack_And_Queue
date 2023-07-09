a9dada796ae2ddbcb3f8080cb4ebd6afc984c85b
Mrk0p <sachinwele23@gamil.com>
2023-07-09 19:55:52 +0530

    Added:welcome to data structure problem

---
+++ src/main/java/com/bridgelabz/stack_and_queue/LinkedListMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.stack_and_queue;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Data Structure problems");
+    }
+}
